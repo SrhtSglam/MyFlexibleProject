3ab17478bbaac6f348c040c8f8359be63a6ba50e
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-11-19 10:15:49 +0300

    Create a blazor project

--- myflexibleproject/myflexibleproject.webapp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace myflexibleproject.webapp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
